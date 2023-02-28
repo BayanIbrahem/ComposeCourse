bca1e47ae244bd0ea3afae82c117b3822a1f8a7c
Bayan <111bemo10.24@gmail.com>
2023-02-28 11:31:13 -0800

    finish Affirmation app and set icon for it.

--- app/src/main/java/com/dev/bayan_ibrahim/composecourse/MainActivity.kt
+++ app/src/main/java/com/dev/bayan_ibrahim/composecourse/MainActivity.kt
@@ -3,13 +3,26 @@ package com.dev.bayan_ibrahim.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dev.bayan_ibrahim.composecourse.domain.model.Affirmation
+import com.dev.bayan_ibrahim.composecourse.domain.model.loadAffirmations
 import com.dev.bayan_ibrahim.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +43,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AffirmationCard(modifier: Modifier = Modifier, affirmation: Affirmation) {
+    Card(
+        modifier = modifier
+            .padding(8.dp),
+        elevation = 4.dp,
+    ) {
+        Column(
+            modifier = modifier,
+        ) {
+            Image(
+                modifier = modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = stringResource(id = affirmation.stringResourceId),
+                style = MaterialTheme.typography.h6
+            )
+        }
+    }
 }
 
+@Composable
+fun AffirmationsList(affirmations: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(affirmations) { affirmation ->
+            AffirmationCard(affirmation = affirmation)
+        }
+    }
+}
+@Preview
+@Composable
+fun AffirmationCardPreview() {
+    ComposeCourseTheme {
+        AffirmationCard(
+            affirmation = Affirmation(
+                stringResourceId = R.string.affirmation1,
+                imageResourceId = R.drawable.image1
+            ),
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun AffirmationsPreview() {
     ComposeCourseTheme {
-        Greeting("Android")
+        AffirmationsList(affirmations = loadAffirmations())
     }
 }
---
+++ app/src/main/java/com/dev/bayan_ibrahim/composecourse/domain/model/Affirmation.kt
@@ -0,0 +1,25 @@
+package com.dev.bayan_ibrahim.composecourse.domain.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.ui.res.stringResource
+import com.dev.bayan_ibrahim.composecourse.R
+
+data class Affirmation (
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int,
+)
+fun loadAffirmations(): List<Affirmation>{
+    return listOf(
+        Affirmation(stringResourceId = R.string.affirmation1, imageResourceId = R.drawable.image1),
+        Affirmation(stringResourceId = R.string.affirmation2, imageResourceId = R.drawable.image2),
+        Affirmation(stringResourceId = R.string.affirmation3, imageResourceId = R.drawable.image3),
+        Affirmation(stringResourceId = R.string.affirmation4, imageResourceId = R.drawable.image4),
+        Affirmation(stringResourceId = R.string.affirmation5, imageResourceId = R.drawable.image5),
+        Affirmation(stringResourceId = R.string.affirmation6, imageResourceId = R.drawable.image6),
+        Affirmation(stringResourceId = R.string.affirmation7, imageResourceId = R.drawable.image7),
+        Affirmation(stringResourceId = R.string.affirmation8, imageResourceId = R.drawable.image8),
+        Affirmation(stringResourceId = R.string.affirmation9, imageResourceId = R.drawable.image9),
+        Affirmation(stringResourceId = R.string.affirmation10, imageResourceId = R.drawable.image10),
+    )
+}
